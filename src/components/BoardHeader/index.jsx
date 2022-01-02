83603888a02e8e1eda7a95b220a125837b014327
raphaelzaneti <raphaelzaneti@gmail.com>
2022-01-02 11:06:12 -0300

    improve board header design

--- src/components/BoardHeader/index.jsx
+++ src/components/BoardHeader/index.jsx
@@ -5,22 +5,23 @@ import './BoardHeader.css'
 export default props =>{
 
     return(
-        <div id="header">
-            <div class="intial-half-header">
-                <button id="dots-btn"><i class="fas fa-ellipsis-h"></i></button>
-                <button id="home"><i class="fas fa-home"></i></button>
-                <button id="boards"><i class="fab fa-trello"></i> <span>Boards</span></button>
-            </div>
+        <div id="header" className="header">
+            <div className="header__intial-half">
+                <button id="dots-btn" className="btn"><i className="fas fa-ellipsis-h"></i></button>
+                <button id="home" className="btn"><i className="fas fa-home"></i></button>
+                <button id="boards" className="btn"><i className="fab fa-trello"></i> <span>Boards</span></button>
             
-            <div id="search">
-                <input type="text" />
-                <button id="search-btn"><i class="fas fa-search"></i></button>
+                <div id="search" className="header__search-area">
+                    <input type="text" className="header__search-input" />
+                    <button id="search-btn" className="btn header__search-btn"><i class="fas fa-search"></i></button>
+                </div>
             </div>
-            <div className="logo-area">
+
+            <div className="header__logo-area">
                 <Logo />
             </div>
-            <div class="final-half-header">
-                <button id="new-board-btn">New Board</button>
+            <div class="header__final-half">
+                <button id="new-board-btn" className="btn">New Board</button>
             </div>
         </div>
     )
