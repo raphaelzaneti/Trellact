dc72fb381835ef5f1a133b9c1f13c88e844437d3
Raphael Haus Zaneti <raphaelzaneti@gmail.com>
2022-10-18 11:47:22 -0500

    add comment in cards front end

--- client/src/hooks/User/useUser.js
+++ client/src/hooks/User/useUser.js
@@ -6,35 +6,11 @@ export const UserContext = createContext()
 export default function UserProvider(props){
     
     const [user, setUser] = useState({})
-
-    const [userList, setUserList] = useState([
-        {
-            id: 1,
-            name: 'General',
-            lastName: 'Admin',
-            photo: 'https://static.onecms.io/wp-content/uploads/sites/28/2021/07/30/rio-de-janeiro-RIOTG0721.jpg',
-        },
-        {
-            id: 2,
-            name: 'Main',
-            lastName: 'User',
-            photo: 'https://blog-www.pods.com/wp-content/uploads/2019/08/MG_6_1_Miami.jpg',
-        },
-        {
-            id: 3,
-            name: 'Second',
-            lastName: 'User',
-            photo: 'https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/large_jpg/eiffel-tower-in-paris-151-medium.jpg?1564742900',
-        }
-
-    ])
     
     return(
         <UserContext.Provider value={{
             user, 
             setUser,
-            userList, 
-            setUserList
          }}>
             {props.children}
         </UserContext.Provider>
@@ -44,7 +20,7 @@ export default function UserProvider(props){
 export function useUser(){
     const context = useContext(UserContext)
 
-    const {user, setUser, userList, setUserList} = context
+    const {user, setUser} = context
 
-    return {user, setUser, userList, setUserList}
+    return {user, setUser}
 }
--- client/src/router/PageRouter.jsx
+++ client/src/router/PageRouter.jsx
@@ -1,12 +1,26 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import BoardRouter from '../components/Board/BoardRouter';
 import Header from '../components/Header/Header';
 import { useBoards } from '../hooks/useBoards/useBoards';
+import { useUser } from '../hooks/User/useUser';
 import Workspace from '../pages/Workspace/Workspace';
 
 function PageRouter() {
 
   const {boards, setBoards, boardId, setBoardId} = useBoards()
+  const {user, setUser} = useUser()
+
+  // Here is a mock user, but in future, it may include the axios call to set the active user, once the login is successful
+  // Probably this call will be made in the login component
+
+  useEffect(() => setUser({
+    user_id: 1,
+    first_name: 'Marshall',
+    last_name: 'Matters',
+    login: 'eminem_123'
+  }), [])
 
   return (
       <Router>
